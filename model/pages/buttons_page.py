d4d4f2b9f721a7c419b2bc946217cd1e75991a08
Roman Shelestov <m9icjkeee@gmail.com>
2023-10-06 19:36:03 +0400

    added readme, fix test

--- model/pages/buttons_page.py
+++ model/pages/buttons_page.py
@@ -8,7 +8,7 @@ class ButtonsPage:
 
     def open(self):
         browser.open('/buttons')
-        browser.all("[id^=google_ads][id$=container__]").with_(timeout=3).wait_until(
+        browser.all("[id^=google_ads][id$=container__]").with_(timeout=10).wait_until(
             have.size_greater_than_or_equal(3)
         )
         browser.all("[id^=google_ads][id$=container__]").perform(command.js.remove)
